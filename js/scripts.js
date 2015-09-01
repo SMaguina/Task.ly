26de0dc6a562746d6f48f05b8be20a00ae0642a0
SMaguina <smag23@gmail.com>
2015-08-31 18:47:59 -0700

    Migration
    Migration from Zeeps repo, update copywriting, optimize images,
    create/update new video header, refactor scripts.js and index.html

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$("#message").on("keyup", function() {
+	var charCount = $("#message").val().length;
+	$("#char-count").html(charCount);
+	if(charCount > 50) {
+		$("#char-count").css("color", "red");	
+	} else {
+		$("#char-count").css("color", "black");
+	}
+}); 
+
+$("button").on("click", function(){
+	var comment = $("#message").val();
+	$("#visible-comment").html(comment);
+});
+
+var map;
+function initialize() {
+  var mapOptions = {
+    zoom: 16,
+    center: new google.maps.LatLng(36.147247, -115.156031),
+    mapTypeId: google.maps.MapTypeId.SATELLITE
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  map.setTilt(45);
+ };
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
+      '&signed_in=true&callback=initialize';
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
